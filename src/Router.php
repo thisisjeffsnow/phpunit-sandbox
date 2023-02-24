7d4e9751f46c58b0a5009c17c809e3cc06b672d1
Jeff Snow <thisisjeffsnow@gmail.com>
2023-02-24 09:27:41 -0500

    Updates Router.php to pass RouterTest.php tests

--- src/Router.php
+++ src/Router.php
@@ -4,4 +4,19 @@ namespace Sandbox;
 
 class Router
 {
+    public function __construct(public array $routeMap = [])
+    {
+    }
+
+    public function addRoute(
+        $requestMethod,
+        $requestPath,
+        $controllerClass,
+        $controllerMethod
+    ) {
+        $this->routeMap[$requestMethod][$requestPath] = [
+            'class' => $controllerClass,
+            'method' => $controllerMethod,
+        ];
+    }
 }
