4c8055361f44292bd980cecf94be81c05366c94c
Jeff Snow <thisisjeffsnow@gmail.com>
2023-02-24 07:33:55 -0500

    modifies Request class to pass test

--- src/Request.php
+++ src/Request.php
@@ -4,6 +4,9 @@ namespace Sandbox;
 
 class Request
 {
+    public const METHOD_POST = 'POST';
+    public const METHOD_GET = 'GET';
+
     public function getPath()
     {
         $rawPath = $_SERVER['REQUEST_URI'];
@@ -12,5 +15,10 @@ class Request
         }
         $delimPos = strpos($rawPath, '?');
         return $delimPos === false ? $rawPath : substr($rawPath, 0, $delimPos);
+    }
+
+    public function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
     }
 }
